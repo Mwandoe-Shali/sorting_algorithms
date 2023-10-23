1989f91eb13d58e4154b2f74136ae4f60ae2484b
Mwandoe Shali <117778682+Mwandoe-Shali@users.noreply.github.com>
2023-10-23 22:50:31 +0300

    Update 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,43 +6,43 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *curr, *behind, *ahead;
-    listint_t *head;
+	listint_t *curr, *behind, *ahead;
+	listint_t *head;
 
-    if (!list || !*list || !(*list)->next)
-        return;
+	if (!list || !*list || !(*list)->next)
+		return;
 
-    head = *list; /* Reference to the head. */
-    curr = head->next; /* Start with second node. */
+	head = *list; /* Reference to the head. */
+	curr = head->next; /* Start with second node. */
 
-    while (curr)
-    {
-        ahead = curr->next;
-        behind = curr->prev;
+	while (curr)
+	{
+		ahead = curr->next;
+		behind = curr->prev;
 
-        while (behind && behind->n > curr->n)
-        {
-            /* Update the next pointer of the node behind to skip curr. */
-            behind->next = curr->next;
-            /* Update the prev pointer of the node after curr, if it exists*/
-            if (curr->next)
-                curr->next->prev = behind;
+		while (behind && behind->n > curr->n)
+		{
+			/* Update the next pointer of the node behind to skip curr. */
+			behind->next = curr->next;
+			/* Update the prev pointer of the node after curr, if it exists*/
+			if (curr->next)
+			curr->next->prev = behind;
 
-            /*Update the prev and next pointers of curr to move it behind.*/
-            curr->prev = behind->prev;
-            curr->next = behind;
+			/*Update the prev and next pointers of curr to move it behind.*/
+			curr->prev = behind->prev;
+			curr->next = behind;
 
-            /* Update the next pointer of node before behind to point to curr*/
-            if (behind->prev)
-                behind->prev->next = curr;
-            else
-                *list = curr;
+			/* Update the next pointer of node before behind to point to curr*/
+			if (behind->prev)
+				behind->prev->next = curr;
+			else
+				*list = curr;
 
-            behind->prev = curr;
-            behind = curr->prev;
-            print_list(*list);
+			behind->prev = curr;
+			behind = curr->prev;
+			print_list(*list);
 
-        }
-        curr = ahead;
-    }
+		}
+		curr = ahead;
+	}
 }
