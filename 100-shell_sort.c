96370a179e6d0c0c0f24f11452f48449c420ed6f
Mwandoe Shali <117778682+Mwandoe-Shali@users.noreply.github.com>
2023-10-24 20:14:23 +0300

    Update 100-shell_sort.c

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -7,39 +7,39 @@
  */
 void shell_sort(int *array, size_t size)
 {
-    size_t gap = 1, i, j;
-    int temp;
+	size_t gap = 1, i, j;
+	int temp;
 
-    if (!array || size < 2)
-        return;
+	if (!array || size < 2)
+		return;
 
-    /* Calculate initial gap using the Knuth sequence */
-    while (gap < size / 3)
-        gap = gap * 3 + 1;
+	/* Calculate initial gap using the Knuth sequence */
+	while (gap < size / 3)
+		gap = gap * 3 + 1;
 
-    while (gap > 0)
-    {
-        for (i = gap; i < size; i++)
-        {
-            /* Store the current element */
-            temp = array[i];
-            j = i;
+	while (gap > 0)
+	{
+		for (i = gap; i < size; i++)
+		{
+			/* Store the current element */
+			temp = array[i];
+			j = i;
 
-            /* Compare and shift elements with the given gap */
-            while (j >= gap && array[j - gap] > temp)
-            {
-                array[j] = array[j - gap];
-                j -= gap;
-            }
+			/* Compare and shift elements with the given gap */
+			while (j >= gap && array[j - gap] > temp)
+			{
+				array[j] = array[j - gap];
+				j -= gap;
+			}
 
-            /* Place the stored element in the right position */
-            array[j] = temp;
-        }
+			/* Place the stored element in the right position */
+			array[j] = temp;
+		}
 
-        /* Reduce the gap for the next iteration */
-        gap = (gap - 1) / 3;
+		/* Reduce the gap for the next iteration */
+		gap = (gap - 1) / 3;
 
-        /* Print the array after each iteration */
-        print_array(array, size);
-    }
+		/* Print the array after each iteration */
+		print_array(array, size);
+	}
 }
